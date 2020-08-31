8241b690f54e77159707632d49443de90a325205
Sean Sundberg <seansund@us.ibm.com>
2020-08-30 23:37:40 -0500

    Removes cluster_type

--- test/stages/stage-key-protect.tf
+++ test/stages/stage-key-protect.tf
@@ -6,7 +6,6 @@ module "dev_key_protect" {
   name_prefix              = var.name_prefix
   provision                = true
   cluster_name             = module.dev_cluster.name
-  cluster_type             = module.dev_cluster.type_code
   cluster_config_file_path = module.dev_cluster.config_file_path
   tools_namespace          = module.dev_capture_state.namespace
 }
