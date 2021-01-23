6015ef75dea80d2aa38bcdd7cdeeb265621d9e02
Sean Sundberg <seansund@us.ibm.com>
2021-01-23 14:26:00 -0600

    Pass ibmcloud_api_key

--- test/stages/stage-key-protect.tf
+++ test/stages/stage-key-protect.tf
@@ -7,4 +7,5 @@ module "dev_key_protect" {
   provision                = true
   admin-access-group       = var.admin_access_group
   user-access-group        = var.user_access_group
+  ibmcloud_api_key         = var.ibmcloud_api_key
 }
